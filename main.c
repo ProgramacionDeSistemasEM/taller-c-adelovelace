c1721035228da79d11f90d00d9a788432a6e6cd2
adelovelace <anmero@fiec.espol.edu.ec>
2021-10-19 15:38:14 -0500

    peso, altura, promedioIMC y maximoIMC

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <math.h>
  
 int main(int argc, char **argv){
 	
@@ -28,12 +29,71 @@ int main(int argc, char **argv){
 	float max_imc = 0.0f;
 	
 
-	//testing 	
+	//declaracion de variable peso y altura
+	float peso = 0.0;
+	float altura = 0.0;
+
+	//arreglo de los elementos 
+	char Peso[elementos];
+	char Altura[elementos];
+
+
+	for(int i =0 ; i < elementos ; i++){
 	
+		printf("Peso:");
+		scanf("%2f",&peso);
+ 		
+	
+		if(peso == 0.0){
+			printf("Numero invalido\n");
+			exit(1);
+		}
+
+	
+		printf("Altura:");
+		scanf("%2f", &altura);
+		
+
+		if (altura == 0.0){
+			printf("Numero invalido\n");
+			exit(1);
+		}
+		
+
+		Peso[i] = peso;
+	      	Altura[i] = altura;	
+	      	
+	 
+	}
+	
+	char IMC[elementos];
+	
+	//calculo del ICM
+	for(int i = 0;i <elementos;i++){
+		IMC[i] = Peso[i] / (Altura[i] * Altura[i]);
+	}
+	
+	float promedioIMC = 0.0;	
+	float maxIMC = 0.0;
+	
+	//promedio del ICM y el maximo ICM
+	for(int i = 0;i <elementos;i++){
+		promedioIMC += IMC[i];
+		
+		if(IMC[i] > IMC[i+1]){
+			maxIMC = IMC[i];
+		}
+	}
+	
+	
+	printf("Promedio IMC: %.2f\n",promedioIMC);
+	printf("Maximo IMC: %.2f\n",maxIMC);
+	
+		
 	
 	//No modifique estas lineas
 	//Guarde los resultados en las 
 	//variables sum y max_imc
 	printf("\npromedio IMC: %.1f\n", sum);
 	printf("maximo IMC: %.1f\n", max_imc);
-}
+}	
